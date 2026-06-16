26ba6e3c71af5ed427215c09f19844d0e9ec4d28
raldagus <raldagus98@gmail.com>
2026-06-16 15:36:13 -0300

    fix: habilitar swagger en produccion

--- Back/Program.cs
+++ Back/Program.cs
@@ -60,12 +60,10 @@ builder.Services.AddCors(options =>
 });
 var app = builder.Build();
 
-// ── Middleware ─────────────────────────────────────────────────
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Stock API v1"));
-}
+// ── Middleware ──────────────
+app.UseSwagger();
+app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Stock API v1"));
+
 app.UseCors("PermitirAngular");
 app.UseHttpsRedirection();
 app.UseAuthorization();
